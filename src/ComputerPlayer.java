d0392c22d383df269bf6010b82205ddda894bae7
Vivian Long <vrlong@uci.edu>
2016-12-12 20:27:27 -0800

    - Add computer move print statement

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -7,6 +7,7 @@ public class ComputerPlayer extends Player {
 			for (int y = 0; y <3; y++) 
 				if (Game.getInstance().getCellInfo(x, y) == null) {
 					Game.getInstance().markCell(x, y);
+					System.out.println("The computer has made its move!");
 					return;
 				}
 	}
